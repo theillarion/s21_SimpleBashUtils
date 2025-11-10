a0af9d8a78a7762c16a4e15995965d5cc90059d2
Cleon Biodiesel <cleonbio@ge-n3.kzn.21-school.ru>
2025-11-10 18:46:34 +0300

    fixed code style

--- src/cat/option.h
+++ src/cat/option.h
@@ -4,12 +4,12 @@
 #include <stdbool.h>
 
 typedef enum s21_option {
-  OPTION_NONE = 0,                  // Опции не выбраны
+  OPTION_NONE = 0,  // Опции не выбраны
   OPTION_NUMBER_NONBLANK = 1 << 0,  // -b: Нумерует только непустые строки
-  OPTION_SHOW_ENDS = 1 << 1,        // -E: Отображает символы конца строки как $
-  OPTION_NUMBER_ALL = 1 << 2,       // -n: Нумерует все выходные строки
+  OPTION_SHOW_ENDS = 1 << 1,  // -E: Отображает символы конца строки как $
+  OPTION_NUMBER_ALL = 1 << 2,  // -n: Нумерует все выходные строки
   OPTION_SQUEEZE_BLANKS = 1 << 3,  // -s: Сжимает несколько смежных пустых строк
-  OPTION_SHOW_TABS = 1 << 4,       // -T: Отображает табы как ^I
+  OPTION_SHOW_TABS = 1 << 4,  // -T: Отображает табы как ^I
   OPTION_SHOW_NONPRINTING =
       1 << 5,  // -v: Отображает все непечаемые символы, кроме $ и ^I
 } t_s21_option;
